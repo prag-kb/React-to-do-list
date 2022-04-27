d2331e077752b7b5c79f38ea0c29cef705da28d8
prag_kb <https://github.com/prag-kb/React-to-do-list.git>
2022-04-27 12:59:54 +0530

    first commit

---
+++ src/ListItems.js
@@ -0,0 +1,35 @@
+import React from "react";
+import App from "./App";
+import './ListItems.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import FlipMove from "react-flip-move";
+
+function ListItems(props) {
+    const items = props.items;
+    const ListItems = items.map(item => {
+        return <div className="list" key={item.key}>
+            <p>
+                <input type="text" id={item.key} value={item.text} onChange={
+                    (e) => {
+                        props.setUpdate(e.target.value, item.key)
+                    }
+                }
+                />
+
+                <span>
+                    <FontAwesomeIcon className='faicons' icon='trash'
+                        onClick={() => props.deleteItem(item.key)} />
+                </span>
+            </p>
+
+        </div>
+    })
+    return (
+        <div>
+            <FlipMove duration={200} easing='ease-in-out'>
+                {ListItems}
+            </FlipMove>
+        </div>
+    )
+}
+export default ListItems;
